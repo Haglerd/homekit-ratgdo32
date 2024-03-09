ratgdo/homekit-ratgdo

Add support for GDO Security +1.0
Squashed commits:
[841aa89] woops left a method out during merge
[0d32e16] fixed crc32 replacement
[08fab5f] merged PR#113 for cache control

also used pure python for build_web_content.py
[4f17141] adjusted try-catch blocks

removed try/catch blocks in checkAuth() so they can be caught outside method.
[153229d] added auth check

added check before post to /setgdo that doesnt have a timeout for time to authenticate
[ccbdb49] added GDO Security +1.0

added GDO Security +1.0
(wedged in)
code cleanup and more testing including existing Security +2.0 needed
[98982e2] Update build_web_content.py

rewrite of build_web_content.py to use all native python code (no gzip/xxd/sed dependencies)

src/utilities.h
@@ -6,4 +6,7 @@ void sync_and_restart();
 uint32_t read_file_from_flash(const char* filename);
 void write_file_to_flash(const char *filename, uint32_t* counter);
 
+uint8_t read_gdo_security_from_flash(const char* filename);
+void write_gdo_security_to_flash(const char *filename, uint8_t* secType);
+
 #endif

Haglerd/homekit-ratgdo32

src/utilities.h
@@ -3,7 +3,7 @@
 #include <stdint.h>
 
 void sync_and_restart();
-uint32_t read_file_from_flash(const char* filename);
-void write_file_to_flash(const char *filename, uint32_t* counter);
+uint32_t read_file_from_flash(const char* filename, uint32_t defaultValue = 0);
+void write_file_to_flash(const char *filename, uint32_t* value);
 
 #endif